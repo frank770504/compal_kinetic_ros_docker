2a0e18855e48b2bc6c9a388c61d4d7d17113f8e5
Yen-Wen Chen <frank770504@gmail.com>
2019-09-27 18:48:25 +0800

    Add scikit-learn

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 touch /test.txt
 # setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-source $ROS_DIR/setup.bash
+if [ -f /opt/ros/$ROS_DISTRO/setup.bash ]; then source "/opt/ros/$ROS_DISTRO/setup.bash"; fi
+if [ -f $ROS_DIR/setup.bash ]; then source $ROS_DIR/setup.bash; fi
+if [ -f $ROS_DIR/devel/setup.bash ]; then source $ROS_DIR/devel/setup.bash; fi
 exec "$@"
-
